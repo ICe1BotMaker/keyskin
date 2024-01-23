98135125e7851c76122cf44b323a7b95feedd665
ICe1BotMaker <ice1github@gmail.com>
2024-01-23 13:47:20 +0900

    [Test] Testing the Chat App

--- tests/default.js
+++ tests/default.js
@@ -1,17 +1,32 @@
 const { default: Keyskin } = require(`../`);
+const fs = require(`fs`);
+const cors = require(`cors`);
+const path = require(`path`);
 
-const keyskin = new Keyskin();
+const keyskin = new Keyskin({ autolog: false });
 
-keyskin.use((req, res, next) => {
-    console.log(req.method);
-    next();
+keyskin.use(cors());
+
+keyskin.on(`get`, `/message/send`, (req, res) => {
+    keyskin.write(`[${req.query.sender}] ${req.query.msg}`);
+
+    const logConfig = JSON.parse(fs.readFileSync(`./log/config.json`, `utf-8`));
+    const log = fs.readFileSync(`./log/${logConfig.latest}`, `utf-8`).replace(/\n/g, `<br>`);
+
+    res.send({ log });
 });
 
-keyskin.on(`get`, `/json`, (req, res) => {
-    res.setHeader(`Content-type`, `application/json`);
-    res.send({ a: 1, b: 2, c: 3 });
+keyskin.on(`get`, `/message/log`, (req, res) => {
+    const logConfig = JSON.parse(fs.readFileSync(`./log/config.json`, `utf-8`));
+    const log = fs.readFileSync(`./log/${logConfig.latest}`, `utf-8`).replace(/\n/g, `<br>`);
+
+    res.send({ log });
+});
+
+keyskin.on(`get`, `/`, (req, res) => {
+    res.sendFile(path.resolve(`${__dirname}/index.html`));
 });
 
 keyskin.listen(8080, () => {
-    console.log(`localhost:${keyskin.port}`);
+    // keyskin.write(`http://localhost:${keyskin.port}`);
 });
